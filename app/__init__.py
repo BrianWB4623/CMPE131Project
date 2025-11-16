d0d5d7c74ed0a0b4ae7cb8e50bb4f26df2653ee4
BrianWB4623 <BrianWB4623@icloud.com>
2025-11-15 22:17:46 -0800

    did the run.py,requirements.txt,.gitignore, and app/__init__.py

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+import os
+
+db=SQLAlchemy()
+login_manager=LoginManager()
+
+basedir=os.path.abspath(os.path.dirname(__file__))
+def create_app():
+    app=Flask(__name__,instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='you-will-never-guess',
+        SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(basedir, 'app.db'),
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    )
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view="auth.login"
+    #blue prints of rpages
+    from app.auth import auth_bp
+    from app.main import main_bp
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(main_bp)
+
+    with app.app_context():
+        db.create_all()
+    return app
+
